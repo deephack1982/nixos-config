67760e65bb7c69b8cb1e03e7b6d40afe0cabc2a7
Mark Dickie <mark.dickie@gmail.com>
2025-09-23 11:19:49 +0200

    Fix typo

--- hosts/frame1/default.nix
+++ hosts/frame1/default.nix
@@ -32,7 +32,7 @@
   services.greetd = {
     enable = true;
     settings.default_session.command = "${pkgs.hyprland}/bin/Hyprland";
-  }
+  };
 
   programs.regreet = {
     enable = lib.mkForce true;
