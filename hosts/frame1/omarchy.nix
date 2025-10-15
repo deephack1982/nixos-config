2405dc530054417badb421a1fa52504cf8c266d9
Mark Dickie <mark.dickie@gmail.com>
2025-10-15 23:44:00 +0200

    Bump omarchy and fix terminal

--- hosts/frame1/omarchy.nix
+++ hosts/frame1/omarchy.nix
@@ -7,6 +7,6 @@
       wallpaper_path = /home/markd/Pictures/Wallpapers/Internals13.jpeg;
     };
     monitors = [ "eDP-1,preferred,auto,1" ",preferred,auto,1" ];
-    terminal = "ghostty";
+    terminal = "kitty";
   };
 }
