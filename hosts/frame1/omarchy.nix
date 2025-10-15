603ed0ac106dc789eef11a39f9404316aa003962
Mark Dickie <mark.dickie@gmail.com>
2025-10-15 17:03:37 +0200

    Change wallpaper for frame1

--- hosts/frame1/omarchy.nix
+++ hosts/frame1/omarchy.nix
@@ -3,6 +3,9 @@
     full_name = "Mark Dickie";
     email_address = "mark.dickie@gmail.com";
     theme = "tokyo-night";
+    theme_overrides = {
+      wallpaper_path = /home/markd/Pictures/Wallpapers/Internals13.jpeg;
+    };
     monitors = [ "eDP-1,preferred,auto,1" ",preferred,auto,1" ];
     terminal = "ghostty";
   };
