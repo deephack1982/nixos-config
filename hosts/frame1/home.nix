53802547acd6da440a50c691eb65ab50299c9d39
Mark Dickie <mark.dickie@gmail.com>
2025-11-09 23:33:09 +0100

    Fix typo

--- hosts/frame1/home.nix
+++ hosts/frame1/home.nix
@@ -95,7 +95,7 @@ in
       GOOGLE_APPLICATION_CREDENTIALS = "/home/markd/Documents/Safe/google.json";
     };
 
-    home.file.".config/nemu/nemu.cfg".source = "${../../confif/nemu.cfg}";
+    home.file.".config/nemu/nemu.cfg".source = "${../../config/nemu.cfg}";
 
     programs.distrobox = {
       enable = true;
